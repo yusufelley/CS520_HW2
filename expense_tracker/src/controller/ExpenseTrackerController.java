4dfaa79ec069f3289bfa9f6d4b4a70f41e63e5b6
Yusuf Elley <yelley2001@gmail.com>
2023-10-15 18:55:48 -0400

    implementing undo transaction pt.1

--- expense_tracker/src/controller/ExpenseTrackerController.java
+++ expense_tracker/src/controller/ExpenseTrackerController.java
@@ -4,12 +4,11 @@ import view.ExpenseTrackerView;
 
 import java.util.List;
 
-
-
 import model.ExpenseTrackerModel;
 import model.Transaction;
+
 public class ExpenseTrackerController {
-  
+
   private ExpenseTrackerModel model;
   private ExpenseTrackerView view;
 
@@ -37,13 +36,20 @@ public class ExpenseTrackerController {
     if (!InputValidation.isValidCategory(category)) {
       return false;
     }
-    
+
     Transaction t = new Transaction(amount, category);
     model.addTransaction(t);
-    view.getTableModel().addRow(new Object[]{t.getAmount(), t.getCategory(), t.getTimestamp()});
+    view.getTableModel().addRow(new Object[] { t.getAmount(), t.getCategory(), t.getTimestamp() });
     refresh();
     return true;
   }
-  
+
   // Other controller methods
+  public boolean removeTransaction(Transaction transaction) {
+    model.removeTransaction(transaction);
+    view.getTableModel().removeRow(0);
+    refresh();
+    return true;
+
+  }
 }
--- expense_tracker/src/view/ExpenseTrackerView.java
+++ expense_tracker/src/view/ExpenseTrackerView.java
@@ -2,8 +2,11 @@ package view;
 
 import javax.swing.*;
 import javax.swing.JFormattedTextField.AbstractFormatterFactory;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
+import controller.ExpenseTrackerController;
 import controller.InputValidation;
 
 import java.awt.*;
@@ -19,16 +22,17 @@ public class ExpenseTrackerView extends JFrame {
   private JFormattedTextField amountField;
   private JTextField categoryField;
   private DefaultTableModel model;
-  
+  private JButton removeTransationBtn; // MARK
 
   public ExpenseTrackerView() {
     setTitle("Expense Tracker"); // Set title
     setSize(600, 400); // Make GUI larger
 
-    String[] columnNames = {"serial", "Amount", "Category", "Date"};
+    String[] columnNames = { "serial", "Amount", "Category", "Date" };
     this.model = new DefaultTableModel(columnNames, 0);
 
     addTransactionBtn = new JButton("Add Transaction");
+    removeTransationBtn = new JButton("Undo Transaction"); // MARK
 
     // Create UI components
     JLabel amountLabel = new JLabel("Amount:");
@@ -37,79 +41,91 @@ public class ExpenseTrackerView extends JFrame {
     amountField = new JFormattedTextField(format);
     amountField.setColumns(10);
 
-    
     JLabel categoryLabel = new JLabel("Category:");
     categoryField = new JTextField(10);
 
     // Create table
     transactionsTable = new JTable(model);
-  
+
+    // Enable single-row selection MARK
+    transactionsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    // Add a selection listener to respond to row selection changes
+    transactionsTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() { // MARK
+      @Override
+      public void valueChanged(ListSelectionEvent e) {
+        if (!e.getValueIsAdjusting()) {
+          handleRowSelection();
+        }
+      }
+    });
+
     // Layout components
     JPanel inputPanel = new JPanel();
     inputPanel.add(amountLabel);
     inputPanel.add(amountField);
-    inputPanel.add(categoryLabel); 
+    inputPanel.add(categoryLabel);
     inputPanel.add(categoryField);
     inputPanel.add(addTransactionBtn);
-  
+
     JPanel buttonPanel = new JPanel();
     buttonPanel.add(addTransactionBtn);
-  
+    buttonPanel.add(removeTransationBtn);
+
     // Add panels to frame
     add(inputPanel, BorderLayout.NORTH);
-    add(new JScrollPane(transactionsTable), BorderLayout.CENTER); 
+    add(new JScrollPane(transactionsTable), BorderLayout.CENTER);
     add(buttonPanel, BorderLayout.SOUTH);
-  
+
     // Set frame properties
     setSize(400, 300);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
-  
+
   }
 
   public void refreshTable(List<Transaction> transactions) {
-      // Clear existing rows
-      model.setRowCount(0);
-      // Get row count
-      int rowNum = model.getRowCount();
-      double totalCost=0;
-      // Calculate total cost
-      for(Transaction t : transactions) {
-        totalCost+=t.getAmount();
-      }
-      // Add rows from transactions list
-      for(Transaction t : transactions) {
-        model.addRow(new Object[]{rowNum+=1,t.getAmount(), t.getCategory(), t.getTimestamp()}); 
-      }
-        // Add total row
-        Object[] totalRow = {"Total", null, null, totalCost};
-        model.addRow(totalRow);
-  
-      // Fire table update
-      transactionsTable.updateUI();
-  
-    }  
-  
+    // Clear existing rows
+    model.setRowCount(0);
+    // Get row count
+    int rowNum = model.getRowCount();
+    double totalCost = 0;
+    // Calculate total cost
+    for (Transaction t : transactions) {
+      totalCost += t.getAmount();
+    }
+    // Add rows from transactions list
+    for (Transaction t : transactions) {
+      model.addRow(new Object[] { rowNum += 1, t.getAmount(), t.getCategory(), t.getTimestamp() });
+    }
+    // Add total row
+    Object[] totalRow = { "Total", null, null, totalCost };
+    model.addRow(totalRow);
 
-  
-  
+    // Fire table update
+    transactionsTable.updateUI();
+
+  }
+
   public JButton getAddTransactionBtn() {
     return addTransactionBtn;
   }
+
   public DefaultTableModel getTableModel() {
     return model;
   }
+
   // Other view methods
-    public JTable getTransactionsTable() {
+  public JTable getTransactionsTable() {
     return transactionsTable;
   }
 
   public double getAmountField() {
-    if(amountField.getText().isEmpty()) {
+    if (amountField.getText().isEmpty()) {
       return 0;
-    }else {
-    double amount = Double.parseDouble(amountField.getText());
-    return amount;
+    } else {
+      double amount = Double.parseDouble(amountField.getText());
+      return amount;
     }
   }
 
@@ -117,12 +133,16 @@ public class ExpenseTrackerView extends JFrame {
     this.amountField = amountField;
   }
 
-  
   public String getCategoryField() {
     return categoryField.getText();
   }
 
   public void setCategoryField(JTextField categoryField) {
     this.categoryField = categoryField;
+  }
+
+  public void handleRowSelection() {
+    int selectedTransaction = transactionsTable.getSelectedRow();
+    ExpenseTrackerController.
   }
 }
