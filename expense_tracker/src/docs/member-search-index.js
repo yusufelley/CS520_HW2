f2b6adf7b9e297c1f1c977d32e5b7bbb757baa94
Siddharth <siddharthjain1220@gmail.com>
2023-10-19 21:05:51 -0400

    Update README.md and generate jdoc

---
+++ expense_tracker/src/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ExpenseTrackerApp","l":"ExpenseTrackerApp()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ExpenseTrackerApp","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
